6c72fb9198468e4ae9d36cc388bdff6ebcfbd139
joellekounde <djokounde@gmail.com>
2024-08-20 09:35:29 +0200

    Démo Flag & Blog

---
+++ src/pages/About.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Navigation from '../components/Navigation';
+import Logo from '../components/Logo';
+
+
+const About = () => {
+    return (
+        <div>
+            <Logo/>
+            <Navigation/>
+            <h1>A propos</h1>
+            <br />
+               <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero aliquam nostrum nobis provident. Distinctio aliquid labore voluptatum animi. Magni ut dolore, tempore, pariatur doloremque aperiam rem et illo voluptatibus consectetur voluptas error distinctio velit porro fugit recusandae ea quo incidunt soluta vero in nam temporibus enim provident. Culpa voluptatum earum ullam a alias eum unde animi delectus laborum autem sit, repellat laudantium odit dolor ducimus nesciunt omnis accusamus. Sequi a ducimus ipsam eos molestiae necessitatibus eaque omnis aperiam. Ipsum possimus eveniet doloribus laudantium fugiat, hic quam, nemo optio consequuntur laboriosam reiciendis mollitia distinctio error illo, voluptatem enim molestias accusantium at!</p>
+               <br />
+               <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores eum animi modi rem eligendi aut impedit magnam voluptatibus, consequuntur porro provident perferendis culpa molestiae iusto libero ea atque corporis suscipit tempore a amet id dolorem laborum? Deleniti esse aliquid asperiores voluptate harum quibusdam. Voluptatibus ratione vel excepturi nesciunt impedit nobis? Fugit, quae. Nostrum inventore dicta odio ducimus illo dolor labore quas distinctio quis aperiam libero odit tempore a sequi reprehenderit, earum id dignissimos in nulla tempora! Eligendi officiis inventore autem fugiat necessitatibus facere, error nesciunt provident ad illo repudiandae recusandae nemo similique deserunt omnis dolores! Maiores veritatis asperiores suscipit architecto dolores necessitatibus repellendus placeat, id eum cupiditate nisi quasi voluptate labore quaerat laborum eos! Optio possimus hic deleniti eum. Temporibus ipsa nihil dolorum ducimus dolores assumenda excepturi incidunt neque itaque reiciendis dignissimos repellat porro et provident, delectus tempore voluptatibus distinctio quidem fugit! Excepturi dolorem cumque, laudantium rerum voluptatibus accusamus magnam minus repudiandae, nemo vitae voluptatum corrupti esse et pariatur? Consequatur numquam et eos illo, aliquam officia reprehenderit voluptate hic non nobis adipisci doloribus exercitationem? Cupiditate, deleniti explicabo! Rem ut sit voluptatum aliquid quasi mollitia dicta cum amet doloribus sunt, quidem molestias eligendi adipisci ipsa fuga sed quaerat neque velit fugit!</p>
+        </div>
+    );
+};
+
+export default About;
